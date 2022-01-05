1d50ea13ce54c60b3b14ead43898f044e39bcf31
amackenzie1 <andrushamackenzie@yahoo.ca>
2022-01-05 14:11:05 -0500

    actually commiting my stuff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,122 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {get_yearly_rate} from './calculator.js'
+
+function myFocus(initial_value){
+  return (props) => {if (props.target.value === initial_value) {
+    props.target.value = ''}}
+}
+
+function myBlur(initial_value){
+  return (props) => {if (props.target.value === '') {
+    props.target.value = initial_value}}
+}
+
+
+function makeForm(){
+  return (
+    <form className='T-Form'> 
+      <input type="text" defaultValue="Amount"
+      onFocus={myFocus("Amount")}
+      className="Amount">
+
+      </input>
+      &nbsp;
+      <input type="date" className="Date">
+
+      </input>
+      &nbsp;
+      <select className="Type">
+        <option>Deposit</option>
+        <option>Withdrawal</option>
+      </select>
+    </form>
+  )
+}
+
 
 function App() {
+  let num = 0;
+  const [inputs, setInputs] = useState([makeForm()]);
+  const [output, setOutput] = useState(-100.78);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* display the value of the output variable if it's not zero */}
+        {/* div should be purple with pink text */}
+        {output !== -100.78 && <div style={{color: "white", fontFamily: "serif"}}>
+          <p>Rate of return: <u>{output}%</u></p>
+        </div>}
+        <div className='start'>
+          <div style={{fontFamily: "serif", fontSize: "20px"}}>
+            Starting Balance</div>
+          {/* a text input form with a default value of "Starting Balance" that disappears on focus */}
+          <input type="text" defaultValue="Starting Balance" 
+            onFocus={myFocus("Starting Balance")}
+            onBlur={myBlur("Starting Balance")}
+            className='Starting Balance'>
+          </input>
+          &nbsp;
+          <input type="date" className='Starting Date'>
+          </input>
+        </div>
+        &nbsp;
+        <div style={{fontFamily: "serif", fontSize: "20px"}}>
+            Ending Balance</div>
+        <div className="end">
+          <input type="text" defaultValue="Ending Balance" 
+            onFocus={myFocus("Ending Balance")}
+            onBlur={myBlur("Ending Balance")}
+            className="Ending Balance">
+          </input>
+          &nbsp;
+          <input type="date" className='Ending Date'>
+          </input>
+        </div>
+        &nbsp;
+        <div style={{fontFamily: "serif", fontSize: "20px"}}>
+            Transactions</div>
+        <div>
+          {inputs}
+        </div>
+        &nbsp;
+        <div>
+          <button onClick={() => {
+              let new_inputs = inputs.concat([makeForm()]);
+              setInputs(new_inputs);
+            }}>
+            Add New Transaction
+          </button> 
+          &nbsp;
+          {/* this button console.logs the value of Starting Balance */ }
+          <button onClick={() => {
+            let results = {};
+            let count = 0;
+            for (let i in document.body.getElementsByClassName("T-Form")){
+              let form = document.body.getElementsByClassName("T-Form")[i];
+              try {
+                results["Transaction" + count] = 
+                  {"Amount": form.getElementsByClassName("Amount")[0].value,
+                    "Date": form.getElementsByClassName("Date")[0].value,
+                    "Type": form.getElementsByClassName("Type")[0].value};
+                count += 1;
+              }
+              catch (err){
+                console.log(err);
+              }
+            }
+            results["Starting Balance"] = document.body.getElementsByClassName("start")[0].getElementsByClassName("Starting Balance")[0].value;
+            results["Starting Date"] = document.body.getElementsByClassName("start")[0].getElementsByClassName("Starting Date")[0].value;
+            results["Ending Balance"] = document.body.getElementsByClassName("end")[0].getElementsByClassName("Ending Balance")[0].value;
+            results["Ending Date"] = document.body.getElementsByClassName("end")[0].getElementsByClassName("Ending Date")[0].value;
+            console.log(results);
+            console.log(results);
+            setOutput(get_yearly_rate(results));
+          }}>
+            Submit
+          </button>
+        </div>
       </header>
     </div>
   );
---
+++ src/calculator.js
@@ -0,0 +1,112 @@
+let results = {
+    "Ending Balance": 1020,
+    "Ending Date": "2023-01-12",
+    "Starting Balance": 1000,
+    "Starting Date": "2022-01-12",
+    "Transaction2": {"Amount": 0, "Date": '2022-06-06', "Type": "Withdrawal"},
+    "Transaction5": {"Amount": 0, "Date": '2022-08-01', "Type": 'Deposit'},
+    "Transaction6": {},
+    "Transaction7": {}
+}
+
+//converts a date of the form 2022-06-06 to a number of days since Jan 1, 1970
+
+function date_to_number(given_date){
+    let date = new Date(given_date);
+    let milliseconds = date.getTime();
+    let days = milliseconds / (1000 * 60 * 60 * 24);
+    return days;
+}
+
+
+function clean(results){
+    
+    for (var key in results){
+        if (key.includes("Transaction")){
+            try {
+                results[key]["Amount"] = parseInt(results[key]["Amount"]);
+            } catch (err){
+                delete results[key];
+            }
+        }
+    }
+    console.log("starting logging");
+    console.log(results);
+    results["Starting Balance"] = parseInt(results["Starting Balance"]);
+    results["Ending Balance"] = parseInt(results["Ending Balance"]);
+
+    let start_day = date_to_number(results["Starting Date"]);
+
+    results["Transactions"] = {};
+    for (var key in results){
+        if (key.includes("Transaction")){
+            if (key === "Transactions"){
+                continue;
+            }
+            let day = date_to_number(results[key]["Date"]) - start_day;
+            if (day in results["Transactions"]){
+                // set factor to be -1 if it's a withdrawal
+                let factor = results[key]["Type"] === "Withdrawal" ? -1 : 1; 
+                results["Transactions"][day] += (results[key]["Amount"] * factor);
+            }
+            else {
+                let factor = results[key]["Type"] === "Withdrawal" ? -1 : 1; 
+                results["Transactions"][day] = (results[key]["Amount"] * factor);
+            }
+            delete results[key];
+        }
+    }
+    delete results["Starting Date"];
+    results["Ending Date"] = date_to_number(results["Ending Date"]) - start_day;
+    return results 
+}
+
+/* we're trying to find the daily rate of return that gives us the ending balance */
+
+function try_rate(rate, results){
+    let money = results["Starting Balance"];
+    let day = 0;
+    while (day < results["Ending Date"]){
+        money *= (1 + rate);
+        if (day in results["Transactions"]){
+            money += results["Transactions"][day];
+        }
+        day += 1;
+    }
+    return money;
+}
+
+function binary_search_for_correct_rate(results){
+    let start = -1;
+    let end = 1;
+    let mid = 0;
+    let rate = 0;
+    while (start < end){
+        mid = (start + end) / 2;
+        rate = try_rate(mid, results);
+        /* stop if the ending balance is within 0.01 of our prediction */
+        if (Math.abs(rate - results["Ending Balance"]) < 0.01){
+            return mid;
+        }
+        else if (rate > results["Ending Balance"]){
+            end = mid;
+        }
+        else {
+            start = mid;
+        }
+    }
+    return mid;
+}
+
+function get_yearly_rate(original_results){
+    //copy original_results into results
+    let results = Object.assign({}, original_results);
+    results = clean(results);
+    let rate = binary_search_for_correct_rate(results);
+    console.log("Rate: " + rate);
+    let yearly = (1 + rate)**365 - 1;
+    console.log("Return: " + yearly);
+    return Math.round(yearly * 1000) / 10;
+}
+
+export { get_yearly_rate };
